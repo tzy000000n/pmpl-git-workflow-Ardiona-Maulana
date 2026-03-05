42b4fde2a9dda3d0cc6fef30847252bbb2ad19ad
Naysilla Salsa <naysillasalsa4@gmail.com>
2026-03-05 14:46:13 +0700

    feat: create welcome alert function

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ console.log("Aplikasi dimulai...");
 function showMessage() {
     alert("Praktikum Git Workflow berjalan.");
 }
+function sambutan() {
+    alert("Selamat datang di project kami!");
+}
 
 console.log("Git Workflow Simulation loaded.");
